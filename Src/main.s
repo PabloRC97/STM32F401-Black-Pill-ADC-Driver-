e407848dfef0bf258cdc88855bafa610bdee7225
PabloERC <pablo.erc079@gmail.com>
2026-03-05 01:39:55 -0600

    Adding some registers

--- Src/main.s
+++ Src/main.s
@@ -7,10 +7,13 @@
 .equ RCC_AHB1ENR,			(RCC_BASE + AHB1ENR_OFFSET)
 .equ GPIOC_BASE, 			0x40020800
 
+.equ APB2ENR_OFFSET,		0x44
+.equ RCC_APB2ENR,			(RCC_BASE + APB2ENR_OFFSET)
+
 .equ MODER_OFFSET, 			0x00
 .equ GPIOC_MODER,			(GPIOC_BASE + MODER_OFFSET)
 
-
+.equ ADC1_EN,				(1<<8)
 .equ GPIOC_ODR_OFFSET,		0x14
 .equ GPIOC_ODR, 			(GPIOC_BASE + GPIOC_ODR_OFFSET)
 
@@ -34,6 +37,8 @@
 .equ IDR_OFFSET, 			0x10
 .equ GPIOA_IDR,				(GPIOA_BASE + IDR_OFFSET)
 
+
+.equ MODER1_ANLG_SLT		0xC
 // Active low switch
 .equ BTN_PRESSED,			0x0000
 .equ BTN_NO_PRESSED,		0x0001
@@ -48,75 +53,25 @@
 				.section 	 .text
 				.global __main
 
-		/* 	LED user 	--> PC13
-			Push botton --> PA0 (connected push button in pull up configuration)
-		*/
+
 __main:
-				bl 	gpio_init
+						/* */
+					/* 1.- Enable clock access to ADC pin's GPIO port*/
 
-loop:
-				bl get_input
-				cmp r0, #BTN_PRESSED
-				beq turn_led_on
-				cmp r0, #BTN_NO_PRESSED
-				beq turn_led_off
-				b 	loop
-
-
-turn_led_on:
-
-				mov r1, #0
-				ldr r2, =GPIOC_BSRR
-				mov r1, #BSRR_13_RESET
-				str r1, [r2]
-				b 	loop
-
-turn_led_off:
-				mov r1, #0
-				ldr r2, =GPIOC_BSRR
-				mov r1, #BSRR_13_SET
-				str r1, [r2]
-				b loop
-
-
-get_input:
-				ldr r1, =GPIOA_IDR
-				ldr r0, [r1]
-				and r0, r0, #BTN_PIN
-				bx lr
-
-gpio_init:
-						//PC13 configuration
-
-				/* Enable clock access to GPIO_C */
-
-				ldr r0, =RCC_AHB1ENR		//Load address of RCC_AHB1ENR into r0
-				ldr r1, [r0]				// Load value at address found in r0 int r1
-				orr r1, #GPIOC_EN
-				str r1, [r0]				// store content in r1 at address found in r0
-
-				/* set PC13 as output */
-				ldr r0, = GPIOC_MODER
-				ldr r1,[r0]
-				orr r1, MODER13_OUTPUT
-				str r1, [r0]
-
-				/* Enable clock access to GPIO_A */
 				ldr r0, =RCC_AHB1ENR		//Load address of RCC_AHB1ENR into r0
 				ldr r1, [r0]				// Load value at address found in r0 int r1
 				orr r1, #GPIOA_EN
 				str r1, [r0]				// store content in r1 at address found in r0
 
-
-				ldr r0, =GPIOA_MODER
+				/* 2.- Set ADC pin PA1 as analog pin  --> PA1 --> ADC1*/
+				ldr r0, = GPIOA_MODER
 				ldr r1, [r0]
-				bic r1, r1, #(3 << 0)
+				orr r1, #MODER1_ANLG_SLT
 				str r1, [r0]
-				bx lr
 
-stop:
-				b	stop
+				* 3.- Enable clock access to the ADC */
+				ldr r0, =RCC_APB2ENR
+				ldr r1, [r0]
+				orr r1, #ADC1_EN
+				st r1, [r0]
 
-
-				.align
-				.end
